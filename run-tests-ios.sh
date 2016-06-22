c714d986d216ed93ceecb1c911610bbdd88cf226
Sean Hammond <sean.hammond@unity3d.com>
2016-06-22 14:17:16 +0300

    poll every second for appium server start message in appium.log for a maximum of 30 seconds

--- run-tests-ios.sh
+++ run-tests-ios.sh
@@ -12,9 +12,25 @@ unzip tests.zip
 
 echo "Starting Appium ..."
 /opt/appium/bin/appium.js -U ${UDID} --command-timeout 120 --log-no-colors --log-timestamp >appium.log 2>&1 &
-sleep 10 # Sleep for appium to launch properly
 
-##### Cloud testrun dependencies end.
+echo -n "Waiting for Appium server to be ready "
+start_string="Appium REST http interface listener started"
+retry=30
+while [ $retry -gt 0 ]
+do
+  sleep 1
+  echo -n "."
+  if [ -n "$(grep -s "$start_string" appium.log)" ]; then
+    retry=0
+    echo " done"
+    echo $(grep "$start_string" appium.log)
+  else
+    ((retry--))
+    if [ $retry -eq 0 ]; then
+      echo " waited 30 seconds but server was not ready"
+    fi
+  fi
+done
 
 export APPIUM_APPFILE=$PWD/application.ipa #App file is at current working folder
 
