4a126b558e98e539a5aeb6c358fc8b79639b4909
Jon Appelberg <jon@unity3d.com>
2017-02-23 17:43:34 +0200

    Removed autostart of appium for both Android and iOS. If your scripts require appium then please start it yourselfe. There's now several versions of appium and not all tests use appium at all. It does not make much sense to auto start it each time.

--- run-tests-ios.sh
+++ run-tests-ios.sh
@@ -6,31 +6,11 @@ export TESTDROID=1
 TEST=${TEST:="your_test.js"} #Name of the test file
 
 ##### Cloud testrun dependencies start
-echo "Extracting tests.zip..."
+echo "[Testdroid-ssa-client] Extracting tests.zip..."
 
 unzip tests.zip
 
-echo "Starting Appium ..."
-/opt/appium/bin/appium.js -U ${UDID} --command-timeout 120 --log-no-colors --log-timestamp >appium.log 2>&1 &
-
-echo -n "Waiting for Appium server to be ready "
-start_string="Appium REST http interface listener started"
-retry=30
-while [ $retry -gt 0 ]
-do
-  sleep 1
-  echo -n "."
-  if [ -n "$(grep -s "$start_string" appium.log)" ]; then
-    retry=0
-    echo " done"
-    echo $(grep "$start_string" appium.log)
-  else
-    ((retry--))
-    if [ $retry -eq 0 ]; then
-      echo " waited 30 seconds but server was not ready"
-    fi
-  fi
-done
+echo "[Testdroid-ssa-client] NOT Starting Appium, start it in your test script if needed!"
 
 export APPIUM_APPFILE=$PWD/application.ipa #App file is at current working folder
 
